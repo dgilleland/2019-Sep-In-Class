0f5879e12df70f8bdc92afdacf0c23a63d012d73
Dan Gilleland <DGilleland@nait.ca>
2019-09-12 13:35:42 -0600

    Reverse generated Entity classes

---
+++ src/Code-First from Database/WestWindSystem/DAL/WestWindContext.cs
@@ -0,0 +1,177 @@
+namespace WestWindSystem.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using WestWindSystem.Entities;
+
+
+    public partial class WestWindContext : DbContext
+    {
+        public WestWindContext()
+            : base("name=WWdb")
+        {
+        }
+
+        public virtual DbSet<Address> Addresses { get; set; }
+        public virtual DbSet<BuildVersion> BuildVersions { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<ManifestItem> ManifestItems { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Payment> Payments { get; set; }
+        public virtual DbSet<PaymentType> PaymentTypes { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Region> Regions { get; set; }
+        public virtual DbSet<Shipment> Shipments { get; set; }
+        public virtual DbSet<Shipper> Shippers { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Territory> Territories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>()
+                .HasMany(e => e.Customers)
+                .WithRequired(e => e.Address)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Address>()
+                .HasMany(e => e.Employees)
+                .WithRequired(e => e.Address)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Address>()
+                .HasMany(e => e.Orders)
+                .WithOptional(e => e.Address)
+                .HasForeignKey(e => e.ShipAddressID);
+
+            modelBuilder.Entity<Address>()
+                .HasMany(e => e.Suppliers)
+                .WithRequired(e => e.Address)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Products)
+                .WithRequired(e => e.Category)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(e => e.Orders)
+                .WithRequired(e => e.Customer)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employee1)
+                .HasForeignKey(e => e.ReportsTo);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Orders)
+                .WithOptional(e => e.Employee)
+                .HasForeignKey(e => e.SalesRepID);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Territories)
+                .WithMany(e => e.Employees)
+                .Map(m => m.ToTable("EmployeeTerritories").MapLeftKey("EmployeeID").MapRightKey("TerritoryID"));
+
+            modelBuilder.Entity<OrderDetail>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Order>()
+                .Property(e => e.Freight)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(e => e.OrderDetails)
+                .WithRequired(e => e.Order)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(e => e.Payments)
+                .WithRequired(e => e.Order)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(e => e.Shipments)
+                .WithRequired(e => e.Order)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Payment>()
+                .Property(e => e.Amount)
+                .HasPrecision(10, 4);
+
+            modelBuilder.Entity<PaymentType>()
+                .Property(e => e.PaymentTypeDescription)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<PaymentType>()
+                .HasMany(e => e.Payments)
+                .WithRequired(e => e.PaymentType)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.ManifestItems)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.OrderDetails)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Region>()
+                .Property(e => e.RegionDescription)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Region>()
+                .HasMany(e => e.Territories)
+                .WithRequired(e => e.Region)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Shipment>()
+                .Property(e => e.FreightCharge)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Shipment>()
+                .Property(e => e.TrackingCode)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Shipment>()
+                .HasMany(e => e.ManifestItems)
+                .WithRequired(e => e.Shipment)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Shipper>()
+                .HasMany(e => e.Shipments)
+                .WithRequired(e => e.Shipper)
+                .HasForeignKey(e => e.ShipVia)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Supplier>()
+                .HasMany(e => e.Products)
+                .WithRequired(e => e.Supplier)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Territory>()
+                .Property(e => e.TerritoryDescription)
+                .IsFixedLength();
+        }
+    }
+}
