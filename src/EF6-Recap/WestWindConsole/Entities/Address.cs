947867abab7abef43c02ab324139815aa1a1a8bc
Dan Gilleland <dgilleland@nait.ca>
2019-09-12 11:49:28 -0600

    Fixes compiler error CS0542

---
+++ src/EF6-Recap/WestWindConsole/Entities/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindConsole.Entities
+{
+    [Table("Addresses")]
+    public class Address
+    {
+        public int AddressId { get; set; }
+
+        [Column("Address")] // maps to the db column Address
+        public string StreetAddress { get; set; }
+
+    }
+}
