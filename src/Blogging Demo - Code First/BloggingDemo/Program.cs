9faeb510c582fdfcea603ac54125ed8ea21a0157
Dan Gilleland <dgilleland@nait.ca>
2019-09-12 10:17:56 -0600

    Added two entity classes

--- src/Blogging Demo - Code First/BloggingDemo/Program.cs
+++ src/Blogging Demo - Code First/BloggingDemo/Program.cs
@@ -12,4 +12,28 @@ namespace BloggingDemo
         {
         }
     }
+
+    namespace Entities
+    {
+        public class Blog
+        {
+            // "Primary Key", by convention
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+            // Navigation Property
+            public virtual ICollection<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            // "Primary Key", by convention
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public int BlogId { get; set; }
+            // Navigation Property
+            public virtual Blog Blog { get; set; }
+        }
+    }
 }
