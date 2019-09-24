460b3cce21935e7e25e4c6ea286a5d80aeb6218b
Dan Gilleland <DGilleland@nait.ca>
2019-09-24 10:59:43 -0600

    Console app to explore C# language

---
+++ src/CSharp Language/GetSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface IPrompt<T>
+    {
+        T Prompt(string message);
+    }
+
+    public class TextInput : IPrompt<string>
+    {
+        public string Prompt(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+
+    public class WholeNumberInput : IPrompt<int>
+    {
+        public int Prompt(string message)
+        {
+            int result;
+            Console.Write(message);
+            string input = Console.ReadLine();
+            while(!int.TryParse(input, out result))
+            {
+                Console.WriteLine("\tThat is not a whole number. Try again.");
+                Console.Write(message);
+                input = Console.ReadLine();
+            }
+            return result;
+        }
+    }
+}
