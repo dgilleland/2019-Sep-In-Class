cee3781a6ca8afc7ec7216b85283b80ce2acde8f
Dan Gilleland <dgilleland@nait.ca>
2019-09-27 09:43:36 -0600

    Create database script

---
+++ DDL/Create Database.sql
@@ -0,0 +1,10 @@
+-- A single-line comment is one that starts with two dashes
+--CREATE DATABASE MyDatabase
+--GO -- Used to indicate that the above code should run as a "batch"
+--DROP DATABASE MyDatabase
+--GO
+
+-- [Ctrl] + r - toggles messages/results window
+USE MyDatabase -- Change the execution context for the script that follows
+GO -- run as a batch
+
