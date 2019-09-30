3fe2710d26234c732f2b871d0841a53a37353ed0
Dan Gilleland <dgilleland@nait.ca>
2019-09-30 09:40:45 -0600

    Create DDL-Practice.sql

---
+++ DDL/DDL-Practice.sql
@@ -0,0 +1,25 @@
+/* **********************************************
+ * Simple Table Creation - Columns and Primary Keys
+ *
+ * File: DDL-Practice.sql
+ * Emergency Service & Product
+ * Specification Document 1
+ * Version 0.0.7
+ *
+ * Author: Dan Gilleland
+ ********************************************** */
+-- Select the CREATE DATABASE stement below to create the demo database.
+-- CREATE DATABASE [ESP-A01]
+USE [ESP-A01] -- this is a statement that tells us to switch to a particular database
+-- Notice in the database name above, it is "wrapped" in square brackets because 
+-- the name had a hypen in it. 
+-- For all our other objects (tables, columns, etc), we won't use hypens or spaces, so
+-- the use of square brackets are optional.
+GO  -- this statement helps to "separate" various DDL statements in our script
+    -- so that they are executed as "blocks" of code.
+
+-- TIP: To refresh this script's knowlege of the DB, Press [ctrl] + [shift] + r
+
+
+
+/* ==================== Practice SQL Below =================== */
