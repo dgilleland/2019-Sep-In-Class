8c12fd4ff3bd2ff4154fe66cb0aa98f82f7f9485
Dan Gilleland <dgilleland@nait.ca>
2019-10-01 11:36:28 -0600

    Re #3 - Read models for nested repeater

---
+++ src/Code-First from Database/WestWindSystem/ReadModels/SupplierProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace WestWindSystem.ReadModels
+{
+    public class SupplierProduct
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string QtyPerUnit { get; set; }
+    }
+}
---
+++ src/Code-First from Database/WestWindSystem/ReadModels/SupplierSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.ReadModels
+{
+    public class SupplierSummary
+    {
+        public string Company { get; set; }
+        public string Contact { get; set; }
+        public string Phone { get; set; }
+        public IEnumerable<SupplierProduct> Products { get; set; }
+    }
+}
