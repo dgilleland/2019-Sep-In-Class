3978c1d249c5fde7cb610875aa553fd9be19e235
Dan Gilleland <dgilleland@nait.ca>
2019-10-22 16:27:04 -0600

    Exploring some OOP

---
+++ src/Remedial OOP/RemedialOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemedialOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var things = new Bunch<string>();
+            things.Add("Dan");
+            things.Add("Don");
+            things.Add("Brad Pitt");
+            Console.WriteLine($"There are {things.Count} names in my list");
+        }
+    }
+}
