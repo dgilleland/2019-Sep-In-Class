037f3581f6832da2a0936a2041baf22c8935de6c
Dan Gilleland <dgilleland@nait.ca>
2019-10-31 13:14:23 -0600

    Notes on the Database Initializer class

--- src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using WebApp.Models;
+
+// You can learn about Database Initialization Strategies in EF6 via
+// http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
 
 namespace WebApp.Admin.Security
 {
-    public class SecurityDbContextInitializer
+    /// <summary>
+    /// Provide functionality for setting up the database for the ApplicationDbContext.
+    /// The specific functionality is to create the database if it does not exist.
+    /// </summary>
+    public class SecurityDbContextInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
     {
     }
 }
