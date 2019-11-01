f969b8ed3af548c6b86bbade92b81e6ca374cd20
Dan Gilleland <DGilleland@nait.ca>
2019-11-01 12:49:47 -0600

    Set up admin user

--- src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
@@ -37,6 +37,26 @@ namespace WebApp.Admin.Security
             #endregion
 
             #region Seed the users
+            // Create an Admin user
+            var adminUser = new ApplicationUser
+            {
+                // TODO: Move hard-coded values to Web.config
+                UserName = "WebAdmin",
+                Email = "Elections2020@Hackers.ru",
+                EmailConfirmed = true
+            };
+
+            // Instantiate the BLL user manager
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            // - The ApplicationUserManager is a BLL class in this website's App_Start/IdentityConfig.cs
+            var result = userManager.Create(adminUser, "Pa$$w0rd"); // TODO: Move password
+            if(result.Succeeded)
+            {
+                // Get the Id that was generated for the user we created/added
+                var adminId = userManager.FindByName("WebAdmin").Id;
+                // Add the user to the Administrators role
+                userManager.AddToRole(adminId, "Administrators");
+            }
             #endregion
             // Note: Keep this call to the base class so it can do its seeding work
             base.Seed(context);
