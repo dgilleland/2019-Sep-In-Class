9289ccf9271b4834adc90302ce9abdde48c946e6
Dan Gilleland <dgilleland@nait.ca>
2019-11-05 16:18:57 -0700

    Replace Hardcoded values

--- src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
@@ -7,6 +7,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using WebApp.Models;
+using static WebApp.Admin.Security.Settings;
 
 // You can learn about Database Initialization Strategies in EF6 via
 // http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
@@ -32,31 +33,29 @@ namespace WebApp.Admin.Security
             // to the design/structure of how we're using Asp.NET Identity.
             // The IdentityRole is an Entity class that represents a security role.
 
-            // TODO: Move these hard-coded security roles to Web.config
-            roleManager.Create(new IdentityRole { Name = "Administrators" });
-            roleManager.Create(new IdentityRole { Name = "Registered Users" });
+            foreach(var role in DefaultSecurityRoles)
+                roleManager.Create(new IdentityRole { Name = role });
             #endregion
 
             #region Seed the users
             // Create an Admin user
             var adminUser = new ApplicationUser
             {
-                // TODO: Move hard-coded values to Web.config
-                UserName = "WebAdmin",
-                Email = "Elections2020@Hackers.ru",
+                UserName = AdminUserName,
+                Email = AdminEmail,
                 EmailConfirmed = true
             };
 
             // Instantiate the BLL user manager
             var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
             // - The ApplicationUserManager is a BLL class in this website's App_Start/IdentityConfig.cs
-            var result = userManager.Create(adminUser, "Pa$$w0rd"); // TODO: Move password
+            var result = userManager.Create(adminUser, AdminPassword);
             if(result.Succeeded)
             {
                 // Get the Id that was generated for the user we created/added
-                var adminId = userManager.FindByName("WebAdmin").Id;
+                var adminId = userManager.FindByName(AdminUserName).Id;
                 // Add the user to the Administrators role
-                userManager.AddToRole(adminId, "Administrators");
+                userManager.AddToRole(adminId, AdminRole);
             }
 
             // Create the other user accounts for all the people in my Demo database
@@ -71,11 +70,11 @@ namespace WebApp.Admin.Security
                     EmailConfirmed = true,
                     PersonId = person.PersonID
                 };
-                result = userManager.Create(user, "Pa$$word1");
+                result = userManager.Create(user, TempPassword);
                 if(result.Succeeded)
                 {
                     var userId = userManager.FindByName(user.UserName).Id;
-                    userManager.AddToRole(userId, "Registered Users");
+                    userManager.AddToRole(userId, UserRole);
                 }
             }
             #endregion
---
+++ src/Custom Identity/WebApp/Admin/Security/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Configuration.ConfigurationManager;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Admin.Security
+{
+    public static class Settings
+    {
+        #region Security Roles
+        public static string AdminRole => AppSettings["adminRole"];
+        /* The above is the same as typing:
+         * public static string AdminRole
+         * {
+         *    get { return ConfigurationManager.AppSettings["adminRole"]; }
+         * }
+         */
+        public static string UserRole => AppSettings["userRole"];
+
+        public static IEnumerable<string> DefaultSecurityRoles =>
+            new List<string> { AdminRole, UserRole };
+        #endregion
+
+        #region Startup Users
+        public static string AdminUserName => AppSettings["adminUserName"];
+        public static string AdminPassword => AppSettings["adminPassword"];
+        public static string AdminEmail => AppSettings["adminEmail"];
+        public static string TempPassword => AppSettings["temporaryUserPassword"];
+        #endregion
+    }
+}
