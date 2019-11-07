aa5d53f1b636bea5931906cfb65663ed29123c71
Dan Gilleland <dgilleland@nait.ca>
2019-11-07 10:48:16 -0700

    Making classes internal

--- src/Custom Identity/DemoSystem/DAL/DemoContext.cs
+++ src/Custom Identity/DemoSystem/DAL/DemoContext.cs
@@ -6,7 +6,7 @@ namespace DemoSystem.DAL
     using System.Linq;
     using DemoSystem.Entities;
 
-    public partial class DemoContext : DbContext
+    internal partial class DemoContext : DbContext
     {
         public DemoContext()
             : base("name=DefaultConnection")
