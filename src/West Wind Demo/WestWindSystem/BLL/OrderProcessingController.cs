0c88a5122fcd44d5bd9dc0c387bd0cb0b137519d
Dan Gilleland <dgilleland@nait.ca>
2019-11-18 15:53:01 -0700

    LoadOrders implementation

--- src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
+++ src/West Wind Demo/WestWindSystem/BLL/OrderProcessingController.cs
@@ -22,7 +22,46 @@ namespace WestWindSystem.BLL
                 //      - Make sure the supplier ID exists, otherwise throw exception
                 //      - [Advanced:] Make sure the logged-in user works for the identified supplier.
                 // Processing
-
+                var result = from ord in context.Orders
+                             where !ord.Shipped // Still items to be shipped
+                                && ord.OrderDate.HasValue // The order has been placed and is ready to ship
+                             select new OutstandingOrder
+                             {
+                                 OrderId = ord.OrderID,
+                                 ShipToName = ord.ShipName,
+                                 OrderDate = ord.OrderDate.Value,
+                                 RequiredBy = ord.RequiredDate.Value,
+                                 OutstandingItems = from detail in ord.OrderDetails
+                                                    where detail.Product.SupplierID == supplierId
+                                                    select new OrderProductInformation
+                                                    {
+                                                        ProductId = detail.ProductID,
+                                                        ProductName = detail.Product.ProductName,
+                                                        Qty = detail.Quantity,
+                                                        QtyPerUnit = detail.Product.QuantityPerUnit,
+                                                        // TODO:
+                                                        //                               Outstanding = (from ship in detail.Order.Shipments
+                                                        //                                             from item in ship.ManifestItems
+                                                        //                                             where item.ProductID == detail.ProductID
+                                                        //                                             select item.ShipQuantity)
+                                                    },
+                                 //        ord.ShipAddressID,
+                                 // Note to self: If there is a ShipTo address, use that, otherwise use the customer address
+                                 FullShippingAddress = ord.ShipAddressID.HasValue
+                                                     ? context.Addresses.Where(x => x.AddressID == ord.ShipAddressID)
+                                                       .Select(a => a.Street + Environment.NewLine +
+                                                                    a.City + Environment.NewLine +
+                                                                    a.Region + " " +
+                                                                    a.Country + ", " +
+                                                                    a.PostalCode).FirstOrDefault()
+                                                     : ord.Customer.Address.Street + Environment.NewLine +
+                                                       ord.Customer.Address.City + Environment.NewLine +
+                                                       ord.Customer.Address.Region + " " +
+                                                       ord.Customer.Address.Country + ", " +
+                                                       ord.Customer.Address.PostalCode,
+                                 Comments = ord.Comments
+                             };
+                return result.ToList();
             }
         }
 
